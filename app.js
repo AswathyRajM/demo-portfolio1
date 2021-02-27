8c0c7224e3b7d8557dc0d2587570fe24916283fa
AswathyRajM <aswathyraj767@gmail.com>
2021-02-27 16:45:04 +0530

    hamburger animation fixed

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const hamburgerIcon = document.querySelector(
   "#navbar .navbar .nav-list .hamburger"
 );
 const navList = document.querySelector("#navbar .navbar .nav-list");
+const menu_item = document.querySelectorAll(
+  "#navbar .navbar .nav-list ul li a"
+);
 
 hamburgerIcon.addEventListener("click", () => {
   navList.classList.toggle("open");
   hamburgerIcon.classList.toggle("active");
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    navList.classList.toggle("open");
+    hamburgerIcon.classList.toggle("active");
+  });
 });
